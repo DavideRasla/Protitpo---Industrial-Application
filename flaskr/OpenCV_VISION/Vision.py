af2022e301c4386a407074a1fc402b718dc9b36e
DavideRasla <davide.rasla@gmail.com>
2020-02-04 11:18:30 +0100

    Vision Works

--- flaskr/OpenCV_VISION/Vision.py
+++ flaskr/OpenCV_VISION/Vision.py
@@ -1,7 +1,7 @@
 import cv2
 import sys
 import requests
-
+import json
 # Load the cascade
 face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
@@ -14,6 +14,14 @@ count = 0
 img = 0
 frame = img
 url = 'http://127.0.0.1:5000/api/get_name'
+addr = 'http://localhost:5000'
+test_url = addr + '/api/get_name'
+
+# prepare headers for http request
+content_type = 'image/jpeg'
+headers = {'content-type': content_type}
+name = ''
+names = ['','','','','','','','','','','']
 
 while True:
     # Read the frame
@@ -26,20 +34,37 @@ while True:
        count+=1
     else:
         count = 0
-      
-    if count > 8:#se per 8 frame riconosco un cambio nel numero di  facce
-       old_faces = len(faces)
-       imencoded = cv2.imencode(".jpg", frame)[1]
-       body = str(imencoded)
-       print('send')
-       x = requests.post(url, data = body)
-       count = 0
-
-
-    print(len(faces))
     # Draw the rectangle around each face
+    print(len(faces))
+    if len(faces) == 0:
+        old_faces = 0
+        for i in range(0, len(names)):
+           names[i] = ''
+   
+    i = 0
     for (x, y, w, h) in faces:
-        cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        rect = cv2.rectangle(img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+        font = cv2.FONT_HERSHEY_PLAIN
+        if count > 10:#se per 10 frame riconosco un cambio nel numero di  facce
+            # encode image as jpeg
+            old_faces = len(faces)
+            _, img_encoded = cv2.imencode('.jpg', img)
+            # send http request with image and receive response
+            response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
+            # decode response
+            print(json.loads(response.text))
+            if 'User Not Found' in json.loads(response.text):
+                old_faces = 0#obbligo a riprovare
+                count = 11
+
+            name = json.loads(response.text)
+            if len(name) < 4:
+                for t in range(0, len(name)):
+                    names[t] = name[t]
+            count = 0
+        cv2.putText(img,names[i],(x, y-10), font, 2,(255,255,255),2,cv2.LINE_AA)
+        i = i + 1
+
     # Display
     cv2.imshow('img', img)
     # Stop if escape key is pressed
--- flaskr/api.py
+++ flaskr/api.py
@@ -6,7 +6,8 @@ from PIL import Image
 from flaskr.face_Def import *
 from flaskr.voice_Def import *
 import wave
-
+import cv2
+import numpy as np 
 from flask import (
     Blueprint, flash, g, redirect, render_template, request, session, url_for, jsonify,
 )
@@ -20,20 +21,26 @@ bp = Blueprint('api', __name__, url_prefix='/api')
 def NameToExternal():
     print('roba')
     if request.method == 'POST':
+
+        r = request
+        # convert string of image data to uint8
+        nparr = np.fromstring(r.data, np.uint8)
+        # decode image
+        img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
+        
         #return jsonify(request.form)
         #salvare il blob in wav nella cartella
         print('eco')
         Filename='flaskr/LogUser/Test_User.jpg'
-        #biteimg = bytes(request.form['file'],encoding="ascii")
-        biteimg = request.form['file'].encode()
-        imgdata = biteimg[biteimg.find(b'/9'):]
-        im = Image.open(io.BytesIO(base64.b64decode(imgdata))).save(filename)
+
+        cv2.imwrite(Filename,img)
 
         Id_User_Verified = Identify_User()
         #Id_User_Verified =  [file for file in glob.glob('./flaskr/LogUser/*.jpg')]
         #Id_User_Verified = cwd = os.getcwd()
+    
         return jsonify(Id_User_Verified)
-    return 
+    return 0 
 
 
 
