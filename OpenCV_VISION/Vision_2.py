a817daf65d4c72622dba9a54b224572650c9da08
DavideRasla <davide.rasla@gmail.com>
2020-02-05 13:31:24 +0100

    d

--- OpenCV_VISION/Vision_2.py
+++ OpenCV_VISION/Vision_2.py
@@ -8,22 +8,25 @@ import threading
 import vlc, easygui
 
 
-name = ''
 # Create a single vlc.Instance() to be share by (possible) multiple players.
 Users_Time_Saved ={  "User1" : {
                         "name" : "",
+                        "name_FILM" : "",
                         "minute" : 0
                     },
                     "User2" : {
                         "name" : "",
+                        "name_FILM" : "",
                         "minute" : 0
                     },
                     "User3" : {
                         "name" : "",
+                        "name_FILM" : "",
                         "minute" : 0
                     },
                     "User4" : {
                         "name" : "",
+                        "name_FILM" : "",
                         "minute" : 0
                     },
                     }
@@ -31,8 +34,9 @@ Num_Faces = 0
 
 
 def Vision_LOOP():
-
         # Load the cascade
+    name = ""
+    print("ciao2")
     face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
     # To capture video from webcam. 
@@ -52,7 +56,7 @@ def Vision_LOOP():
     headers = {'content-type': content_type}
     #global name = ''
     names = ['','','','','','','','','','','']
-
+    print("cai")
     while True:
         # Read the frame
         _, img = cap.read()
@@ -79,27 +83,27 @@ def Vision_LOOP():
             font = cv2.FONT_HERSHEY_PLAIN
             if count > 10:#se per 10 frame riconosco un cambio nel numero di  facce
                 # encode image as jpeg
+                count = 0
                 old_faces = len(faces)
                 _, img_encoded = cv2.imencode('.jpg', img)
                 # send http request with image and receive response
                 response = requests.post(test_url, data=img_encoded.tostring(), headers=headers)
-                # decode response
+              #  decode response
                 print(json.loads(response.text))
                 if 'User Not Found' in json.loads(response.text):
                     old_faces = 0#obbligo a riprovare
                     count = 11
 
                 name = json.loads(response.text)
-                print("il nome è:", name)
-                if len(name) < 4:
-                    for t in range(0, len(name)):
-                        names[t] = name[t]
-                        Users_Time_Saved['User1']['name'] = name[t] #Salvo nella struttura condivisa 
-                count = 0
-            cv2.putText(img,names[i],(x, y-10), font, 2,(255,255,255),2,cv2.LINE_AA)
-            i = i + 1
+                print("il nome e':", name)
+            if len(name) < 4:
+                for t in range(0, len(name)):
+                    names[t] = name[t]
+                    Users_Time_Saved['User1']['name'] = name[t] #Salvo nella struttura condivisa 
 
-        # Display
+        cv2.putText(img,names[i],(x, y-10), font, 2,(255,255,255),2,cv2.LINE_AA)
+        i = i + 1
+
         cv2.imshow('img', img)
         # Stop if escape key is pressed
         k = cv2.waitKey(30) & 0xff
@@ -109,33 +113,43 @@ def Vision_LOOP():
     cap.release()
  
 def VLC_LOOP():
-    media = easygui.fileopenbox(title="Choose media to open")
-    player = vlc.MediaPlayer(media)
-    image = "bigles.gif"
-    while True:
-        choice = easygui.buttonbox(title="@biglesp Audio Player",msg="Press Play to start",image=image,choices=["Play","Pause","Stop","New"])
-        print(choice)
-        if choice == "Play":
-            if Users_Time_Saved['User1']['minute'] != 0:
-                player.set_time(float(Users_Time_Saved['User1']['minute']))
-            player.play()
-        elif choice == "Pause":
-            player.pause()
-        elif choice == "Stop":
-            Users_Time_Saved['User1']['minute'] = player.get_position()
-            print("il tempo è:", Users_Time_Saved['User1']['minute'])
-            player.stop()
-        elif choice == "New":
-            media = easygui.fileopenbox(title="Choose media to open")
-            player = vlc.MediaPlayer(media)
-        else:
-            break
-    
+    Users_Time_Saved['User1']['name_FILM'] = "./File_Video_2.mp4"
+    Users_Time_Saved['User1']['name'] = "Dave" 
+    if Users_Time_Saved['User1']['name'] != '' and  Users_Time_Saved['User1']['name_FILM'] != '' :
+        #se l'utente e' stato riconosciuto e il nome preso dal database
+        Film_Name =  Users_Time_Saved['User1']['name_FILM'] 
+
+
+        #print(media) 
+        #media = easygui. fileopenbox(title="Choose media to open")
+        vlc_instance = vlc.Instance('--no-audio', '--fullscreen')
+        player = vlc_instance.media_player_new()
+        media = vlc_instance.media_new(Film_Name)
+        player.set_media(media)
+        while True:
+            choice = easygui.buttonbox(title="Vision 2.0",msg="Press Play to start",choices=["Play","Pause","Stop","New"])
+            print(choice)
+            if choice == "Play":
+            # if Users_Time_Saved['User1']['minute'] != 0:
+                #    player.set_time(float(Users_Time_Saved['User1']['minute']))
+                player.play()
+            elif choice == "Pause":
+                player.pause()
+            elif choice == "Stop":
+                Users_Time_Saved['User1']['minute'] = player.get_position()
+                print("il tempo è:", Users_Time_Saved['User1']['minute'])
+                player.stop()
+            elif choice == "New":
+                media = easygui.fileopenbox(title="Choose media to open")
+                player = vlc.MediaPlayer(media)
+            else:
+                break
+
 if __name__ == "__main__":
     # creating thread
     t1 = threading.Thread(target=Vision_LOOP, args=())
     t2 = threading.Thread(target=VLC_LOOP, args=())
- 
+    print("ciao")
     # starting thread 1
     t1.start()
     # starting thread 2
